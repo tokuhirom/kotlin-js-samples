7de318c32d90aa130c322d3a948061cd3bcbeb70
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-05-30 14:50:22 +0900

    call preload functions

--- 03-electron/renderer/src/jsMain/kotlin/renderer.kt
+++ 03-electron/renderer/src/jsMain/kotlin/renderer.kt
@@ -1,16 +1,40 @@
+import js.promise.Promise
 import kotlinx.browser.document
+import kotlinx.browser.window
 import web.timers.setInterval
 import kotlin.time.Duration.Companion.seconds
 
-fun main() {
-    val hoge = Hoge("hoge", 33)
-    document.getElementById("root")?.let {root ->
-        setInterval(1.seconds) {
-            println("invoked timer...")
-            val div = document.createElement("div")
-            div.textContent = "Hello, World! WOWYAY!! AGGKOKO $hoge"
-            root.appendChild(div)
+external interface Versions {
+    fun node(): String
+    fun chrome(): String
+    fun electron(): String
+    fun ping(): Promise<String>
+}
+
+suspend fun main() {
+    try {
+        val hoge = Hoge("hoge", 33)
+
+        val versions = window.asDynamic().versions as Versions
+
+        println("Node version: ${versions.node()}")
+        println("Chrome version: ${versions.chrome()}")
+        println("Electron version: ${versions.electron()}")
+
+        versions.ping().then {
+            println("Ping response(): $it")
         }
+
+        document.getElementById("root")?.let {root ->
+            setInterval(1.seconds) {
+                println("invoked timer...")
+                val div = document.createElement("div")
+                div.textContent = "Hello, World! WOWYAY!! ASAGE $hoge"
+                root.appendChild(div)
+            }
+        }
+        println("Hello, electron World! $hoge!! mjs!! ん")
+    } catch (e: Throwable) {
+        console.error(e)
     }
-    println("Hello, electron World! $hoge!! mjs!! ん")
 }
