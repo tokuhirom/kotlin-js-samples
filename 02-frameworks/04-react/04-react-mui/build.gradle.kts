4f66461dd2517e1a74862370d35613a871fa479e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-06-18 11:52:30 +0900

    update wrapper to 756

--- 02-frameworks/04-react/04-react-mui/build.gradle.kts
+++ 02-frameworks/04-react/04-react-mui/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
     sourceSets {
         val jsMain by getting {
             dependencies {
-                implementation(project.dependencies.platform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.755"))
+                implementation(project.dependencies.platform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.756"))
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
